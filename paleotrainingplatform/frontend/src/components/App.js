1af24cbd73de2ca9a7447ebd886b7ddbd7fdae36
Christopher Yang <chrisyang365@gmail.com>
2019-11-21 01:41:47 -0800

    created split pane feature

--- paleotrainingplatform/frontend/src/components/App.js
+++ paleotrainingplatform/frontend/src/components/App.js
@@ -2,12 +2,19 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import Axios from "axios";
 import ImageScoring from "./ImageScoring";
+import SplitterLayout from "react-splitter-layout";
+import "react-splitter-layout/lib/index.css";
 
 function App() {
   return (
-    <div>
-      <ImageScoring />
-    </div>
+    <SplitterLayout percentage secondaryInitialSize={25} secondaryMinSize={25}>
+      <div className="my-pane">
+        <ImageScoring />
+      </div>
+      <div className="my-pane">
+        <h2>2nd Pane</h2>
+      </div>
+    </SplitterLayout>
   );
 }
 
