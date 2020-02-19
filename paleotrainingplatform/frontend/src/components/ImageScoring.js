9ba33d4e9a205e6d398f082362c21ff4367512c1
Christopher Yang <christopheryang@Christophers-MacBook-Pro.local>
2020-02-18 23:55:37 -0800

    data distribution now works

--- paleotrainingplatform/frontend/src/components/ImageScoring.js
+++ paleotrainingplatform/frontend/src/components/ImageScoring.js
@@ -1,38 +1,60 @@
 import React, { useEffect, useState } from "react";
-import { Loader, Label, Grid, Form } from "semantic-ui-react";
+import { Loader, Label, Grid, Form, Checkbox } from "semantic-ui-react";
 import ReactImageMagnify from "react-image-magnify";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Axios from "axios";
 
 const ImageScoring = props => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [images, setImages] = useState(null);
-  const [lesionTypeOptions, setLesionTypeOptions] = useState(null);
   const [lesionScores, setLesionScores] = useState({})
+  // const [checked, setChecked] = useState(null);
+  const lesionTypeOptions = [
+    { value: "Type 0", text: "Absence of lesions" },
+    { value: "Type A", text: "Type A" },
+    { value: "Type B", text: "Type B" },
+    { value: "Type C", text: "Type C" },
+    { value: "Type D", text: "Type D" },
+    { value: "Type E1", text: "Type E1" },
+    { value: "Type E2", text: "Type E2" }
+  ]
+  const [lesionTypes, setLesionTypes] = useState(null);
   const lesionActivityOptions = [
     { value: "1", text: "active" },
     { value: "2", text: "healed" },
     { value: "3", text: "mixed" }
   ];
 
+  const lesionTypeIndex = {
+    "Type 0": "1",
+    "Type A": "2",
+    "Type B": "3",
+    "Type C": "4",
+    "Type D": "5",
+    "Type E1": "6",
+    "Type E2": "7",
+  }
+
   useEffect(() => {
     if (!isLoaded) {
       Axios.get(`http://127.0.0.1:8000/api/training/lesionImage/`).then(
         result => {
-          setImages(result.data);
           if (result.data) {
-            Axios.get(`http://127.0.0.1:8000/api/training/lesionType/`).then(
-              response => {
-                var options = []
-                for (var i = 0; i < response.data.length; i++) {
-                  options.push({value: response.data[i].name, text: response.data[i].name})
-                }
-                setLesionTypeOptions(options)
-                if (response.data) {
-                  setIsLoaded(true)
-                }
-              }
-            )
+            // for (var i = 0; i < result.data.length; i++) {
+            //   for (var j = 0; j < lesionTypeOptions.length; j++) {
+            //     checked[result.data[i].image_url][lesionTypeOptions[j].value] = false;
+            //   }
+            // }
+            // Axios.get(`http://127.0.0.1:8000/api/training/lesionType/`).then(
+            //   response => {
+            //     setLesionTypes(response.data)
+            //     if (response.data) {
+            //       setIsLoaded(true)
+            //     }
+            //   }
+            // )
+            setImages(result.data);
+            setIsLoaded(true);
           }
         }
       );
@@ -40,8 +62,23 @@ const ImageScoring = props => {
   }, [isLoaded]);
 
   function handleSubmit() {
-    // console.log(lesionScores)
+    for (const [imageURL, type] of Object.entries(lesionScores)) {
+      console.log(type)
+      Axios.post(`http://127.0.0.1:8000/api/training/lesionScore/`, {
+        image_url: imageURL,
+        score: type
+      }).then(function (response) {
+        console.log(response)
+      })
+        .catch(function (error) {
+          console.log(error)
+        })
+    }
   }
+
+  // checkboxChangeHandler = (data) => {
+  //   checked[data.name][data.label] = data.checked;
+  // }
 
   return (
     <div>
@@ -88,13 +125,28 @@ const ImageScoring = props => {
                 <br />
                 <br />
                 <Grid centered>
-                  <Form.Select 
-                  fluid
-                  onChange={(e, data) => {
-                    const imageURL = image.image_url
-                    lesionScores[imageURL] = data.value
-                  }}
-                  options={lesionTypeOptions} 
+                  <Form.Select
+                    fluid
+                    onChange={(e, data) => {
+                      // if (data.value == "More than one lesion type present") {
+                      //   return (<Form.Group>
+                      //     <label>Please select multiple lesion types if applicable:</label>
+                      //     {lesionTypeOptions.map(option => {
+                      //       <Checkbox
+                      //         label={option.text}
+                      //         name={image.image_url}
+                      //         checked={checked[image.image_url][option.value]}
+                      //         onChange={(e, data) => checkboxChangeHandler(data)} />
+                      //     })}
+                      //   </Form.Group>)
+                      // } else {
+                      //   const imageURL = image.image_url
+                      //   lesionScores[imageURL] = data.value
+                      // }
+                      const imageURL = image.image_url
+                      lesionScores[imageURL] = data.value
+                    }}
+                    options={lesionTypeOptions}
                   />
                 </Grid>
                 <br />
@@ -123,13 +175,13 @@ const ImageScoring = props => {
               scores: lesionScores,
               lesionImages: images
             }
-            }}>
-          <Form.Button onClick={() => handleSubmit()}>Submit</Form.Button>
+          }}>
+            <Form.Button onClick={() => handleSubmit()}>Submit</Form.Button>
           </Link>
         </Form>
       ) : (
-        <Loader active />
-      )}
+          <Loader active />
+        )}
     </div>
   );
 };
--- paleotrainingplatform/training/urls.py
+++ paleotrainingplatform/training/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from .api import LesionImageViewSet, LesionReferenceViewSet, LesionTypeViewSet, HealingReferenceViewSet
+from .api import LesionImageViewSet, LesionReferenceViewSet, LesionTypeViewSet, HealingReferenceViewSet, LesionScoreViewSet
 
 router = routers.DefaultRouter()
 router.register('api/training/lesionImage',
@@ -11,6 +11,10 @@ router.register('api/training/lesionType',
                 LesionTypeViewSet, 'lesion_type')
 router.register('api/training/healingReference',
                 HealingReferenceViewSet, 'healing_references')
+router.register('api/training/healingReference',
+                HealingReferenceViewSet)
+router.register('api/training/lesionScore',
+                LesionScoreViewSet, 'lesion_score')
 
 urlpatterns = [
     path('', include(router.urls))
