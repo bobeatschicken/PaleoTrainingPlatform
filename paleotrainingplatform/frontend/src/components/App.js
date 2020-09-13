a079c57f209148b7740327191c655f25623eb211
Christopher Yang <chrisyang365@gmail.com>
2020-09-13 14:32:52 -0700

    added homepage layout

--- paleotrainingplatform/frontend/src/components/App.js
+++ paleotrainingplatform/frontend/src/components/App.js
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom";
 import Axios from "axios";
 import ImageForm from "./ImageForm";
 import Results from "./Results";
+import HomepageLayout from "./HomePage";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-        <Router>
-          <Switch>
-            <Route exact path="/" component={ImageForm} />
-            <Route exact path="/results" component={Results} />
-          </Switch>
-        </Router>
+    <Router>
+      <Switch>
+        <Route exact path="/home" component={HomepageLayout} />
+        <Route exact path="/quiz" component={ImageForm} />
+        <Route exact path="/results" component={Results} />
+      </Switch>
+    </Router>
   );
 }
 
